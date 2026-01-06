6899899115b2d2f495150e0da981b757e6efc83a
Eugene <etngo@outlook.com>
2026-01-05 21:23:05 -0800

    test

--- chrono/scripts/player.gd
+++ chrono/scripts/player.gd
@@ -3,8 +3,12 @@ extends CharacterBody2D
 
 const SPEED = 120.0
 const JUMP_VELOCITY = -300.0
+const REWIND_DURATION := 4.0
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var rewind_buffer: Array = []
+@onready var is_rewinding := false
+@onready var rewind_index := 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +18,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("rewind") and rewind_buffer.size() < 0:
+		start_rewind()
 
 	# Get the input direction
 	var direction := Input.get_axis("move_left", "move_right")
@@ -39,3 +46,37 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	record_state(delta)
+
+func record_state(delta: float) -> void:
+	rewind_buffer.append({
+		"position": global_position,
+		"velocity": velocity,
+		"flip": animated_sprite.flip_h,
+	})
+
+	# Keep only last 4 seconds worth of data
+	var max_frames := int(REWIND_DURATION / delta)
+	if rewind_buffer.size() > max_frames:
+		rewind_buffer.pop_front()
+
+func start_rewind() -> void:
+	is_rewinding = true
+	rewind_index = rewind_buffer.size() - 1
+
+func rewind_step() -> void:
+	if rewind_index < 0:
+		stop_rewind()
+		return
+
+	var state = rewind_buffer[rewind_index]
+	global_position = state.position
+	velocity = state.velocity
+	animated_sprite.flip_h = state.flip
+
+	rewind_index -= 1
+
+func stop_rewind() -> void:
+	is_rewinding = false
+	rewind_buffer.clear()
